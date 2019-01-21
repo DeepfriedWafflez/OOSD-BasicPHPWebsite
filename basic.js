e74777a9629f589cce5aa0bb19f2aae1a755869e
Brent Ward <mr.brent.ward@gmail.com>
2019-01-21 14:15:21 -0700

    Added basic.js

---
+++ basic.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+
+var topNav = document.getElementById("topNav");
+var sticky = topNav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topNav.classList.add("sticky")
+  } else {
+    topNav.classList.remove("sticky");
+  }
+}
