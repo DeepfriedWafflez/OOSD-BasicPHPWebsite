864e3e399e808a28dcf25337a4c26a6665845452
Brent Ward <mr.brent.ward@gmail.com>
2019-01-22 13:35:24 -0700

    Added script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+//Sticky Navigation bar
+window.onscroll = function() {myFunction()};
+var topNav = document.getElementById("topNav");
+var sticky = topNav.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    topNav.classList.add("sticky")
+  } else {
+    topNav.classList.remove("sticky");
+  }
+}
+
+//Submit Button Confirmation
+var submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click", function(event)
+{
+  var choice = confirm("Are you sure you want to submit the form?");
+  if(choice=true)
+  {
+    console.log("Data Submitted")
+  }else{
+    console.log("Data declined");
+  }
+});
+
+//Reset Button Confirmation
+var resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", function(event)
+{
+  var choice = confirm("Are you sure you want to reset the form?");
+  if(choice=true)
+  {
+    console.log("Data Reset")
+  }else{
+    console.log("Data not Reset");
+  }
+});
+
+
