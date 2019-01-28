abb536e02205521931242e82b836a8e0d63e925d
Brent Ward <mr.brent.ward@gmail.com>
2019-01-27 21:17:49 -0700

    updated for 8th assignment

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Sticky Navigation bar
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 var topNav = document.getElementById("topNav");
 var sticky = topNav.offsetTop;
 
@@ -10,58 +10,241 @@ function myFunction() {
     topNav.classList.remove("sticky");
   }
 }
-/*
-//Submit Button Confirmation
-var submitButton = document.getElementById("submitButton");
-submitButton.addEventListener("click", function(event)
-{
-  var choice = confirm("Are you   sure you want to submit the form?");
-  if(choice=true)
-  {
-    console.log("Data Submitted")
-  }else{
-    console.log("Data declined");
+
+//Photo Album widget -- only runs on index body
+if (document.getElementById("indexBody")) {
+  var photoAlbum = document.getElementsByTagName('section')[0];
+  var table = document.createElement('table');
+
+  //Album Arrays
+  var descs = [
+    "If you enjoy sunbathing along the coasts of the most beautiful beaches in the world, you will love our packages!",
+    "Vist south America for some of the most amazing, lush forests. Side benefit of awesome ziplines!",
+    "Looking for a romantic getaway? Spain will give you and your siginificant other memories to cherish forver.",
+    "Italy, the home of the Roman colliseum, and the city of Venice, is a beautiful place to take the entire family!"];
+  var images = [
+    "Images/Travel1.jpg",
+    "Images/Travel2.jpg",
+    "Images/Travel3.jpg",
+    "Images/Travel4.jpg"];
+  var links = [
+    "https://www.tripadvisor.ca/TravelersChoice-Beaches-cTop-g1",
+    "https://www.visitcostarica.com/en",
+    "https://www.spain.info/en/",
+    "https://www.rome.net/"];
+
+
+  //add links inside the for loop images[i].addEventListener
+
+
+
+  //Adds image and matching desc to a tablerow, then adds the row to the table
+  for (var i = 0; i < images.length; i++) {
+    var row = document.createElement('tr');
+    var col1 = document.createElement('td');
+    var col2 = document.createElement('td');
+    var image = document.createElement('img');
+
+    image.src = images[i];
+    col1.appendChild(image);
+
+    //adds link to the image
+
+    curLink = links[i];
+    col1.addEventListener("click", function (event) {
+      var newWindow = window.open(curLink);
+      function windowClose() {
+        newWindow.close();
+      }
+      setTimeout(windowClose, 5000);
+    });
+
+
+
+    //adds data to table
+    row.appendChild(col1);
+    col2.innerHTML = descs[i];
+    row.appendChild(col2);
+    table.appendChild(row);
   }
-});
-
-//Reset Button Confirmation
-var resetButton = document.getElementById("resetButton");
-resetButton.addEventListener("click", function(event)
-{
-  var choice = confirm("Are you sure you want to reset the form?");
-  if(choice=true)
-  {
-    console.log("Data Reset")
-  }else{
-    console.log("Data not Reset");
-  }
-});
-
-*/
-
-var photoAlbum = document.getElementsByTagName('section')[0];
-var table = document.createElement('table');
-
-var img1 = document.createElement('img');
-img1.src = "Images/Travel1.jpg";
-var img2 = document.createElement('img');
-img2.src = "Images/Travel2.jpg";
-var img3 = document.createElement('img');
-img3.src = "Images/Travel3.jpg";
-var img4 = document.createElement('img');
-img4.src = "Images/Travel4.jpg";
-
-var images = [img1, img2, img3, img4];
-var descs = ['photo1', 'photo2', 'photo3', 'photo4'];
-
-var x = descs.length;
-//Adds image and matching desc to a tablerow, then adds the row to the table
-for(var i = 0; i < x; i++) {
-  var newRow = document.createElement('tr');
-  newRow.appendChild(images[i]);
-  var tdText = newRow.appendChild(document.createElement('td'));
-  tdText.innerHTML = descs[i];
-  table.appendChild(newRow);
+  console.log(table);
+  photoAlbum.appendChild(table);
 }
-console.log(table);
-photoAlbum.appendChild(table);
+
+
+//only runs if it has a form body
+if (document.getElementById("registerBody") || document.getElementById("contactBody") || document.getElementById("feedbackBody")) {
+
+
+  //Submit Button Confirmation
+  /*
+  var submitButton = document.getElementById("submitButton");
+  var myForm = document.myForm;
+  submitButton.addEventListener("click", function (event) {
+    var choice = confirm("Are you sure you want to submit the form?");
+    if (choice = true) {
+     document.myForm.submit();
+    }
+  });*/
+
+  //Reset Button Confirmation
+  var resetButton = document.getElementById("resetButton");
+  resetButton.addEventListener("click", function (event) {
+    var choice = confirm("Are you sure you want to reset the form?");
+    if (choice = true) {
+      document.myForm.reset();
+    }
+  });
+}
+
+//tool tip for register page
+if (document.getElementById("registerBody")) {
+
+  //tool tip variables
+  var myForm = document.myForm;
+  var email = document.myForm.email;
+  var firstName = document.myForm.firstName;
+  var lastName = document.myForm.lastName;
+  var password = document.myForm.password;
+  var address = document.myForm.address;
+  var postalCode = document.myForm.postalCode;
+  var cardNumber = document.myForm.cardNumber;
+
+  //tooltip listeners- has a focus and blur event listener
+  //email tooltip
+  email.addEventListener("focus", function (event) {
+    document.getElementById("emailTT").style.visibility = "visible";
+  })
+  email.addEventListener("blur", function (event) {
+    document.getElementById("emailTT").style.visibility = "hidden";
+  })
+  //first name tooltip
+  firstName.addEventListener("focus", function (event) {
+    document.getElementById("firstNameTT").style.visibility = "visible";
+  })
+  firstName.addEventListener("blur", function (event) {
+    document.getElementById("firstNameTT").style.visibility = "hidden";
+  })
+  //lastname tooltip
+  lastName.addEventListener("focus", function (event) {
+    document.getElementById("lastNameTT").style.visibility = "visible";
+  })
+  lastName.addEventListener("blur", function (event) {
+    document.getElementById("lastNameTT").style.visibility = "hidden";
+  })
+  //password tooltip
+  password.addEventListener("focus", function (event) {
+    document.getElementById("passwordTT").style.visibility = "visible";
+  })
+  password.addEventListener("blur", function (event) {
+    document.getElementById("passwordTT").style.visibility = "hidden";
+  })
+  //address tooltip
+  address.addEventListener("focus", function (event) {
+    document.getElementById("addressTT").style.visibility = "visible";
+  })
+  address.addEventListener("blur", function (event) {
+    document.getElementById("addressTT").style.visibility = "hidden";
+  })
+  //postal code tooltip
+  postalCode.addEventListener("focus", function (event) {
+    document.getElementById("postalCodeTT").style.visibility = "visible";
+  })
+  postalCode.addEventListener("blur", function (event) {
+    document.getElementById("postalCodeTT").style.visibility = "hidden";
+  })
+  //card number tooltip
+  cardNumber.addEventListener("focus", function (event) {
+    document.getElementById("cardNumberTT").style.visibility = "visible";
+  })
+  cardNumber.addEventListener("blur", function (event) {
+    document.getElementById("cardNumberTT").style.visibility = "hidden";
+  })
+}
+
+//check for empty fields on register page
+if (document.getElementById("registerBBody")) {
+  var myForm = document.myForm;
+
+  //empty field variables
+  var errorEmail = document.getElementById("errorEmail");
+  var errorFN = document.getElementById("errorfirstName");
+  var errorLN = document.getElementById("errorLastName");
+  var errorPW = document.getElementById("errorPassword");
+  var errorAdd = document.getElementById("errorAddress");
+  var errorCity = document.getElementById("errorCity");
+  var errorPC = document.getElementById("errorPostalCode");
+  var errorCN = document.getElementById("errorCardNumber");
+
+  var submitButton = document.getElementById("submitButton");
+
+  submitButton.addEventListener("click", function (event) {
+    //asks if youre sure (part of previous assignment)
+    var choice = confirm("Are you sure you want to submit the form?");
+    if (choice = true) {
+      document.myForm.submit();
+    }
+    //hiding all displays
+    errorEmail.style.display = "none";
+    errorFN.style.display = "none";
+    errorLN.style.display = "none";
+    errorPW.style.display = "none";
+    errorAdd.style.display = "none";
+    errorCity.style.display = "none";
+    errorPC.style.display = "none";
+    errorCN.style.display = "none";
+
+    //collecting values
+    var email = document.myForm.email.value;
+    var firstName = document.myForm.firstName.value;
+    var lastName = document.myForm.lastName.value;
+    var password = document.myForm.password.value;
+    var address = document.myForm.address.value;
+    var city = document.myForm.city.value;
+    var postalCode = document.myForm.postalCode.value;
+    var cardNumber = document.myForm.cardNumber.value;
+    console.log("email");
+    //Pops up with message
+    if (!email) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!firstName) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!lastName) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!password) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!address) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!city) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!postalCode) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    if (!cardNumber) {
+      event.preventDefault();
+      errorName.style.display = "block";
+      console.log("missing email");
+    }
+    
+  })
+}
